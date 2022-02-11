5f483e7bfd976fcbe07012ec684a0bc5d9cf52e0
cNGUYEN <35670719+TNg018@users.noreply.github.com>
2022-02-11 17:13:28 -0500

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,4 +1,4 @@
-//Chloe Nguyen
+//cNguyen
 //2.10.2022 Data Structures
 //Bubble sort
 
@@ -9,23 +9,22 @@ class BubbleSort{
 		for (int i=0; i < arrLength-1;i++) {
 			for (int j=0;j<arrLength-i-1; j++) {
 				if (arr[j+1]<arr[j]) {				//if left element is less than right element,
-					int temp = arr[j]; 				//swap array elements
+					int temp = arr[j]; 			//swap array elements
 					arr[j]=arr[j+1];				
 					arr[j+1]=temp;
 				}
 			}
 		}
 	}   
-	public void printArray(int arr[], int len)
-    {
-        for (int i=0; i<len; ++i) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+	public void printArray(int arr[], int len){
+        	for (int i=0; i<len; ++i) {
+           		System.out.print(arr[i] + " ");
+        	}
+        	System.out.println();
+    	}
 	public static void main(String args[]) {
 		BubbleSort bs = new BubbleSort();
-		int[] arr = {90,84,37,21,34,19,9};
+		int[] arr = {90,84,37,21,34,19,9};				
 		int length = arr.length;
 		System.out.print("Given Array: ");
 		bs.printArray(arr, length);
