2d0efc68e0c8c474bd5cb2610afd4f4ba2dff03e
cNGUYEN <35670719+TNg018@users.noreply.github.com>
2022-02-11 17:16:21 -0500

    Update LoopSum.java

--- LoopSum.java
+++ LoopSum.java
@@ -7,7 +7,7 @@ public class LoopSum {
   public static void main(String[] args) {
         display_LoopSum(11,10);
   }
-  private static void display_LoopSum(int a, int b) {
+  private static void displayLoopSum(int a, int b) {
   	int sum = 0;
   	if (a > b) {
   		for (int i = b; i<=a; i++) {
